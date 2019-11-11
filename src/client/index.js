8651cdb89e662f8e28f90a07d628b237e5771f13
Kelly Cho <wenhsiu55@gmail.com>
2019-11-11 23:11:08 +0800

    Image API set.

--- src/client/index.js
+++ src/client/index.js
@@ -1,16 +1,18 @@
 import { generateEntry } from './js/generateEntry';
 import { getLocation } from './js/generateEntry';
 import { getWeather } from './js/generateEntry';
+import { getImage } from './js/generateEntry';
 import { postData } from './js/generateEntry';
 import { updateUI } from './js/generateEntry';
+import { addListener } from './js/generateEntry';
 
-document.getElementById('generate').addEventListener('click', generateEntry);
 
 import './styles/sm-screens.scss';
 import './styles/md-screens.scss';
 import './styles/lg-screens.scss';
 import './styles/style.scss';
 
+addListener();
 
 export {
 
--- src/client/js/generateEntry.js
+++ src/client/js/generateEntry.js
@@ -2,6 +2,9 @@ const newEntry = {};
 
 // get weather info from the api based on the city name entered
 function generateEntry(e) {
+
+	document.getElementById('display').innerHTML = 'Loading...';
+
 	const genoUrl = 'http://api.geonames.org/searchJSON?q=';
 	const username  = '&maxRows=10&username=udacity';
 
@@ -9,6 +12,9 @@ function generateEntry(e) {
 	// source: https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9
 	const dskyUrl = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/';
 	const apikey = 'a4617a54dc1452d58ec269b461e2d4c5/';
+
+	const imgUrl = 'https://pixabay.com/api/?key=14231861-b5b623e0f265e9e5bcfccc7a4&q=';
+	const mediaType = '&image_type=photo';
 
 	// create a new date instance dynamically with JS
 	let d = new Date();
@@ -61,8 +67,11 @@ function generateEntry(e) {
 	})
 	.then(function() {
 		updateUI();
+	})
+	.then (function() {
+		getImage(imgUrl, newEntry.city, mediaType);
+		document.getElementById('display').innerHTML = '';
 	});
-
 }
 
 // async GET LOCATION function
@@ -86,6 +95,21 @@ const getWeather = async(baseUrl, key, location) => {
 		return data;
 	} catch(error) {
 		console.log('error in getWeather()', error);
+	}
+}
+
+// async GET IMAGE function
+const getImage = async(baseUrl, location, type) => {
+	console.log(baseUrl + location + type)
+	const response = await fetch(baseUrl + location + type);
+
+	try {
+		const data = await response.json();
+		document.getElementById('img').src = data.hits[0].webformatURL;
+		return data;
+		
+	} catch(error) {
+		console.log('error in getImage()', error);
 	}
 }
 
@@ -113,16 +137,14 @@ const postData = async(url = '', data = {}) => {
 // async UPDATE UI function
 const updateUI = async() => {
 	const request = await fetch('http://localhost:8000/record');
-	alert('update');
 
 	try {
 		const record = await request.json();
 
-		document.getElementById('date').innerHTML = 'Date: ' + record.from + ' - ' + record.to;
-		document.getElementById('city').innerHTML = 'City: ' + record.city;
-		document.getElementById('country').innerHTML = 'Country: ' + record.country;
+		document.getElementById('date').innerHTML = record.from + ' - ' + record.to;
+		document.getElementById('location').innerHTML = 'City: ' + record.city + ', ' + record.country;
 		document.getElementById('weather').innerHTML = 'Weahter: ' + record.weather;
-		document.getElementById('temp').innerHTML = 'Temperature: ' + record.low + '&degC - ' + record.high + '&degC';
+		document.getElementById('temp').innerHTML = 'Temperature: ' + record.low + '&degF - ' + record.high + '&degF';
 
 		// reset enter fields
 		document.getElementById('input-city').value = '';
@@ -134,10 +156,16 @@ const updateUI = async() => {
 	}
 }
 
+function addListener() {
+	document.getElementById('generate').addEventListener('click', generateEntry);
+}
+
 export {
 	generateEntry,
 	getLocation,
 	getWeather,
+	getImage,
 	postData,
-	updateUI
+	updateUI,
+	addListener
 }
